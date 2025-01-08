49173288a960a90a86b008c7d0677939937e9d62
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-08 23:26:16 +0200

    Interface Example 02

--- Session4OOP/Program.cs
+++ Session4OOP/Program.cs
@@ -1,11 +1,24 @@
-﻿using Session4OOP.Interface_Example1;
+﻿using Session4OOP.Interface_Example_02;
+using Session4OOP.Interface_Example1;
 
 namespace Session4OOP
 {
     internal class Program
     {
+        static void PrintTenNumberFromSeries(ISeries series)
+        {
+            if (series is null) return;
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write($"{series.Current}\t");
+                series.GetNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
+            #region Interface Example 01
             IType reference;
             //declare for reference of type IType, contains garbage value
             //this reference can refer to an object of any type implementing IType interface
@@ -14,6 +27,17 @@ namespace Session4OOP
             reference.MyProperty = 10;
             reference.MyMethod();
             reference.print();
+            #endregion
+
+            #region Interface Example 02
+            TypeB seriesByTwo = new TypeB();
+            PrintTenNumberFromSeries(seriesByTwo);
+
+            TypeC seriesByThree = new TypeC();
+            PrintTenNumberFromSeries(seriesByThree);
+
+
+            #endregion
         }
     }
 }
