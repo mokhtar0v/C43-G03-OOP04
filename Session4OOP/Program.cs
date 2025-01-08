2ca3c36b4c9623e8b4535ab5c9fe672dc6206302
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-08 11:16:27 +0200

    Add project files.

---
+++ Session4OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session4OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
