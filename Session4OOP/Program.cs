fcc0c1edb38cbbb4550f46ec7cffbb563d878e8f
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-09 11:58:23 +0200

    Shallow Copy vs Deep Copy

--- Session4OOP/Program.cs
+++ Session4OOP/Program.cs
@@ -49,6 +49,34 @@ namespace Session4OOP
             IFlyable flyable = airplane;
             flyable.Forward();
             #endregion
+
+            #region Shallow Copy vs Deep Copy
+            #region Shallow Copy
+            int[] arr01 = [1, 2, 3];
+            int[] arr02 = [4, 5, 6];
+            Console.WriteLine($"{arr01.GetHashCode()}, {arr02.GetHashCode()}");
+            arr02 = arr01; Console.WriteLine("After Shallow copy arr01->arr02");
+            // this object [1,2,3] has now two references (arr01,arr02)
+            //[4,5,6] has became unreachable object
+            //this called shallow copy
+            Console.WriteLine($"{arr01.GetHashCode()}, {arr02.GetHashCode()}");
+            arr02[0] = 100;
+            Console.WriteLine(arr01[0]);
+            #endregion
+            #region Deep Copy
+            int[] arr03 = [1, 3, 5];
+            int[] arr04 = [2, 4, 6];
+            Console.WriteLine($"{arr01.GetHashCode()}, {arr02.GetHashCode()}");
+            arr03 = (int[])arr04.Clone(); Console.WriteLine("Deep Copy arr03 -> (int[])arr04.Clone()");
+            // deep copy
+            //clone method will generate new object with new and different identity
+            //this object will have same state (data) of the caller object
+            Console.WriteLine($"{arr03.GetHashCode()}, {arr04.GetHashCode()}");
+            arr04[0] = 100;
+            Console.WriteLine(arr03[0]);
+
+            #endregion
+            #endregion
         }
     }
 }
